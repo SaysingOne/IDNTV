baf5ac08fa25aa2b84f8948e890d5aeb73f91677
SaysingOne <sats271988@gmail.com>
2022-11-21 19:03:40 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+byte a = 10;
+int b = 0;
+b = a;
+Console.WriteLine(b);
+
+
+int c = 255;
+float d = 0f;
+d = c;
+Console.WriteLine(d);
+
+float g = 10.5f;
+int h = 0;
+h = (int)g;
+
+
+Console.ReadLine();
